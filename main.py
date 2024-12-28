d4211deec338f402148c0c23a1a35f624018db81
Aditya Mitra <adityaarghya0@gmail.com>
2024-12-28 12:30:31 +0530

    Feature addition: Shell commands

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ from dona_dev import DonaDev
 if __name__ == "__main__":
     ai_dev = AIDev()
     dona_dev = DonaDev(
-        token='YOUR_TELEGRAM_TOKEN',
-        chat_id='YOUR_CHAT_ID',
+        token='BOT TOKEN',
+        chat_id='CHAT ID',
         ai_dev=ai_dev,
         data_dir='data_dir',
         git_token='YOUR_GITHUB_PAT',
         ollama_host='localhost', 
-        ollama_port=11434
+        ollama_port=11434,
+        custom_commands={
+            'shutdown': 'shutdown -s -f -t 0',
+            'lock pc': 'Rundll32.exe user32.dll,LockWorkStation',
+            'hello world': 'echo Hello friend'
+        }
     )
     dona_dev.main_loop1()
